bac616e67133b567dc3424df7204404b40c4ee5a
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-10-23 20:51:29 +0300

    Added endpoint for get workers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import psycopg2
 from dotenv import dotenv_values
+from pydantic import BaseModel
 
 config = dotenv_values(".env")
 
@@ -16,7 +17,35 @@ cursor = connect.cursor()
 
 app = FastAPI()
 
+class vm_get_workers(BaseModel):
+    id: int
+    firstName: str
+    position: str
+    phone: str
+
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Start Server"}
+
+
+@app.get("/get-workers")
+def get_workers():
+    try:
+        cursor.execute("""
+            SELECT firstName, position, phone
+            FROM worker;
+        """)
+        result = cursor.fetchall()
+        list_workers = []
+        for worker in result:
+            list_workers.append({
+                "firstName": worker[0],
+                "position": worker[1],
+                "phone": worker[2]
+            })
+
+        return {"workers": list_workers}
+    except Exception as e:
+        return {"error": e}
+
