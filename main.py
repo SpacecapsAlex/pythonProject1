131f788f9d314850b2cfcd0ce3bc94cfead1f252
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-10-23 21:07:08 +0300

    Added endpoint for get worker information by id

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ class vm_get_workers(BaseModel):
     phone: str
 
 
+class vm_get_worker(BaseModel):
+    id: int
+    firstName: str
+    surName: str
+    email: str
+    phone: str
+    address: str
+    position: str
+    salary: float
+    departmentName: str
+
+
 @app.get("/")
 def root():
     return {"message": "Start Server"}
@@ -48,6 +60,32 @@ def get_workers():
             ))
 
         return {"workers": list_workers}
+    except:
+        return {"error": traceback.format_exc()}
+
+
+@app.get("/get-worker/{worker_id}")
+def get_worker(worker_id: int):
+    try:
+        cursor.execute(f"""
+            SELECT w.id, w.firstName, w.surName, w.email, w.phone, w.address, w.position, w.salary, d.name
+            FROM worker w
+            LEFT JOIN department d ON w.department_id = d.id
+            WHERE w.id = {worker_id};
+        """)
+        result = cursor.fetchone()
+        worker = vm_get_worker(
+            id=result[0],
+            firstName=result[1],
+            surName=result[2],
+            email=result[3],
+            phone=result[4],
+            address=result[5],
+            position=result[6],
+            salary=result[7],
+            departmentName=result[8]
+        )
+        return {"worker": worker}
     except:
         return {"error": traceback.format_exc()}
 
